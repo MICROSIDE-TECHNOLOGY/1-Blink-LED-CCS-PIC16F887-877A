15be35fba7679c1e605080d6f6043a492d9ad1bf
vhperaltaj <victor.peralta@microside.com>
2023-08-29 13:44:30 -0600

    Cleaned identation, removed system_init

--- Proyecto/PIC16F877A/Blink led.c
+++ Proyecto/PIC16F877A/Blink led.c
@@ -1,7 +1,12 @@
-/*
-AUTOR: MICROSIDE TECHNOLOGY S.A. DE C.V.
-FECHA: JUNIO 2019
-*/
+/************************************************************************************************
+Company:
+Microside Technology Inc.
+File Name:
+Blink led.c
+Product Revision  :  1
+Device            :  X-TRAINER
+Driver Version    :  1.0
+************************************************************************************************/
 
 /*
 ---------------------------------------------------------------------------
@@ -10,30 +15,18 @@ en la tarjeta X-TRAINER
 ---------------------------------------------------------------------------
 */
 
-#include <16F877A.h>                            //Incluye el microcontrolador con el que se va a trabajar 
-#use delay(clock=20Mhz, crystal)                //Tipo de oscilador y frecuencia dependiendo del microcontrolador 
+#include <16F877A.h>                //Incluye el microcontrolador con el que se va a trabajar 
+#use delay(clock=20Mhz, crystal)    //Tipo de oscilador y frecuencia dependiendo del microcontrolador 
 
+#define LED PIN_A1                  //Pin donde está conectado el LED del X-TRAINER
 
-#define LED PIN_A1                              //Pin donde está conectado el LED del X-TRAINER
-
-void SisInit(void)
-  {
-      set_tris_a(0XFD);                         //Pin RA1 como salida
-  }
-
-void main(void)
-
-  {
-     SisInit ();
+void main(void) {
+   set_tris_a(0xFD);                //Pin RA1 como salida
      
-     while(1)
-
-           {
-                  output_HIGH (LED);            //Cambio de estado en el pin RA1
-                  delay_ms (500);               //Retardo
-                  output_LOW (LED);             //Cambio de estado en el pin RA1     
-                  delay_ms (500) ;              //Retardo
-           }
+   while(1) {
+      output_HIGH (LED);            //Cambio de estado en el pin RA1
+      delay_ms (500);               //Retardo
+      output_LOW (LED);             //Cambio de estado en el pin RA1     
+      delay_ms (500) ;              //Retardo
+   }
 }
-
- 
